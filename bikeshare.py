f2de61c3402c0f682d013f2ce125dc6aea499887
shahad_jeza <shoodialamri@gmail.com>
2022-12-20 21:10:59 +0300

    style

--- bikeshare.py
+++ bikeshare.py
@@ -200,6 +200,8 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+            
+        print('thank you ! , godbuy !!')
 
 
 if __name__ == "__main__":
